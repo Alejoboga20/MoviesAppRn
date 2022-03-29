084a691e6785eb395d6fb798303005642369cc89
Alejo Boga <alejoboga20@gmail.com>
2022-03-29 14:07:57 -0300

    Implementing carousel

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { ActivityIndicator, Text, View } from 'react-native';
+import { ActivityIndicator, Dimensions, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { MoviePoster } from '../components/MoviePoster';
 import { useMovies } from '../hooks/useMovies';
+import Carousel from 'react-native-snap-carousel';
+
+const { width: windowWidth } = Dimensions.get('window');
 
 export const HomeScreen = () => {
   const { top } = useSafeAreaInsets();
@@ -17,7 +20,14 @@ export const HomeScreen = () => {
 
   return (
     <View style={{ marginTop: top + 20 }}>
-      <MoviePoster movie={nowPlayingMovies[0]} />
+      <View style={{ height: 440 }}>
+        <Carousel
+          data={nowPlayingMovies}
+          renderItem={({ item }) => <MoviePoster movie={item} />}
+          sliderWidth={windowWidth}
+          itemWidth={300}
+        />
+      </View>
     </View>
   );
 };
