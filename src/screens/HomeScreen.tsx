bb3779e753e55e7dd7b7bbf0b0290053029eb455
Alejo Boga <alejoboga20@gmail.com>
2022-04-03 11:19:39 -0300

    Create helper to get colors

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -5,8 +5,8 @@ import { MoviePoster } from '../components/MoviePoster';
 import { useMovies } from '../hooks/useMovies';
 import { HorizontalSlider } from '../components/HorizontalSlider';
 import { GradientBackground } from '../components/GradientBackground';
+import { getImageColors } from '../helpers/getImageColors';
 import Carousel from 'react-native-snap-carousel';
-import ImageColors from 'react-native-image-colors';
 
 const { width: windowWidth } = Dimensions.get('window');
 
@@ -17,8 +17,9 @@ export const HomeScreen = () => {
   const getPosterColors = async (index: number) => {
     const movie = nowPlaying[index];
     const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-    const colors = await ImageColors.getColors(uri, {});
-    console.log(colors);
+
+    const [primary, secondary] = await getImageColors(uri);
+    console.log([primary, secondary]);
   };
 
   if (isLoading)
